29ebc92ca0ccf9103bb7546ea33ece57d630e614
wonwhiK <kohop8888@gmail.com>
2024-12-08 15:22:49 +0900

    [CHORE]

--- run_evaluation.py
+++ run_evaluation.py
@@ -3,7 +3,7 @@ import json
 
 
 res_list = []
-model_list = ["final_model/BaseModel", "final_model/LoRA"]
+model_list = ["final_model/BaseModel", "final_model/LoRA", "final_model/PreFixTuning"]
 for i in model_list :
     res_list.append(evaluation.main(model_name=i))
 
