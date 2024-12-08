e31b3d747b12fd4dba6d5f0dd8713ff1965a8c1f
wonwhiK <kohop8888@gmail.com>
2024-12-08 21:39:48 +0900

    [FIX]

--- run_evaluation.py
+++ run_evaluation.py
@@ -9,10 +9,10 @@ res_list = []
 model_list = ["BaseModel", "LoRA", "PreFixTuning"]
 for i in model_list :
     model_nm = "final_model/" + i
-    lora_checkpoint = "checkpoint/" + i
+    lora_checkpoint = "final_model/" + i
     prefix_config = None
     if i == "PreFixTuning" :
-        prefix_config = "checkpoint/" + i
+        prefix_config = "final_model/" + i
         lora_checkpoint = None
     res_list.append(evaluation.main(model_name=model_nm, lora_checkpoint=lora_checkpoint, prefix_config= prefix_config))
     lora_checkpoint = None
