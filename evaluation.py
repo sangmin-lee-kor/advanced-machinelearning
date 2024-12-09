b332d60254b899f76ea0f93ebd1f5e4e46e32ecc
wonwhiK <kohop8888@gmail.com>
2024-12-09 19:25:15 +0900

    [CHORE]

--- evaluation.py
+++ evaluation.py
@@ -161,6 +161,7 @@ def evaluation(data, model_nm, tokenizer, model, generation_config, device) :
         input_ids = inputs['input_ids'].to(device)
         
         res = []
+        print(device)
         print("Input IDs device:", inputs["input_ids"].device)
         print("Model device:", next(model.parameters()).device)
 
